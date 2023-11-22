9f16881e5321dfd7c1325b54cd50b5cc561febed
baikinsoo <baikinsoo95@gmail.com>
2023-11-23 00:52:37 +0900

    업로드 Re

---
+++ src/test/java/hello/uploadRe/UploadReApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.uploadRe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
